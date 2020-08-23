cc458a944cfd2a465ddf329b09e6acf7cbf50f70
robajillo <rjillo@yahoo.com>
2020-08-23 20:15:34 +0300

    modified vote component. ts and .html and added buttons for upvote and downvote

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
-  styleUrls: ['./vote.component.css']
+  styleUrls: ['./vote.component.css'],
 })
 export class VoteComponent implements OnInit {
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  constructor() { }
+  uvotes = 0;
+  dvotes = 0;
 
-  ngOnInit(): void {
+  upVote() {
+    this.uvotes = this.uvotes + 1;
   }
 
+  downVote() {
+    this.dvotes = this.dvotes + 1;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
