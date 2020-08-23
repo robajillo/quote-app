78fbf278698e85a0755178ec6a4c417de05c948c
robajillo <rjillo@yahoo.com>
2020-08-23 15:06:47 +0300

    added an incraement and decreament and also a few quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,14 +1,40 @@
 
 import { Component, OnInit } from '@angular/core';
-import {Quote} from './../quote'
+import { Quote } from '../quote';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
+  quotes = [
+    new Quote("Life", "Abraham Lincon","character is like a tree and reputation like a shadow.The shadow is what we think of it"),
+    new Quote("Life", "Albert Einsten", "A person who never made a mistake never tried anything new"),
+    new Quote("Life", "Unknown", "In understanding be men")
+  ]
+  previousNo: number
+  lastNo: number
+  token: number
+
+  addQuote(chosenQuote) {
+    this.quotes.push(chosenQuote)
   }
+
+  upvote(i) {
+    this.quotes[i].upvotes + 1
+  }
+  downvote(i) {
+    this.quotes[i].downvotes + 1
+  }
+  delQuote(i) {
+    this.quotes.splice(i, 1)
+  }
+
+
+constructor() { }
+
+ngOnInit() {
+}
 
 }
